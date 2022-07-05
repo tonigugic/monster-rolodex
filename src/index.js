bf79b3f6e8a80f19f6363da052f41d02efbc9e8b
tonigugic <toni.gugic@gmail.com>
2022-07-05 13:46:13 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App text="Test" />
   </React.StrictMode>
 );
 
